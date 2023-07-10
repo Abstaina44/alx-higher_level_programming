0919e168a2bc19e35b8691f99a306d8ecc3edd1a
Abstaina44 <padcliff88@gmail.com>
2023-07-10 06:12:01 -0700

    A function which returns a linked List

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
+"""This program return a list withh the methods of the obj"""
+
+
 def lookup(obj):
-    attributes = []
-    methods = []
-
-    # Get the list of attributes
-    for attr in dir(obj):
-        if not callable(getattr(obj, attr)):
-            attributes.append(attr)
-
-    # Get the list of methods
-    for attr in dir(obj):
-        if callable(getattr(obj, attr)):
-            methods.append(attr)
-
-    # Return the combined list of attributes and methods
-    return attributes + methods
+    """Gets the methods of the obj"""
+    return dir(obj)

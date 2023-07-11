589a40bf65807f33c103a191b3e1dfdfc3a77454
Abstaina44 <padcliff88@gmail.com>
2023-07-11 10:12:40 -0700

    A class Student that define

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
-"""
-This program take the file add_item.json, and add the
-parameters to the list inside this file.
-- If the file doesn't exist create it.
-- If no exist parameters do nothing or create the list if the file is empty.
-"""
 
-from sys import argv
-from os.path import exists
+"""Class to JSON"""
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-namefile = "add_item.json"
-argc = len(argv)
+def class_to_json(obj):
+    """
+    a function that returns the dictionary description with
+    simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object
+    args:
+        obj: is an instance of a Class
+        All attributes of the obj Class are serializable:
+    """
 
-file_list = []
-
-if exists(namefile):
-    file_list = load_from_json_file(namefile)
-
-if (argc == 1):
-    save_to_json_file(file_list, namefile)
-else:
-    for index in range(1, argc):
-        file_list.append(argv[index])
-    save_to_json_file(file_list, namefile)
+    return obj.__dict__

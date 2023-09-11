172c4521cfc3e21b14c4213c332aaa6a8b7051ff
Abstaina44 <padcliff88@gmail.com>
2023-09-11 22:06:19 +0000

    A script that computes and prints a factorial

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,8 +2,8 @@
 
 const { argv } = require('process');
 const num = parseInt(argv[2]);
-const printC = (cuantity) => {
-  for (; cuantity > 0; cuantity--) console.log('C is fun');
+const printC = (quantity) => {
+  for (; quantity > 0; quantity--) console.log('C is fun');
 };
 
 Number.isInteger(num) ? printC(num) : console.log('Missing number of occurrences');

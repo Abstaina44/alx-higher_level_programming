6ba15c6569a5c5215cb2acd41b248eadfe31aa15
Abstaina44 <padcliff88@gmail.com>
2023-11-02 06:29:34 +0000

    Javascript J query Task

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $('INPUT#btn_translate').click(function () {
+    const lang = $('#language_code').val();
+    $.ajax({
+      url: `https://hellosalut.stefanbohacek.dev/?lang=${lang}`,
+      type: 'GET',
+      dataType: 'json'
+    }).done(function (data) {
+      $('DIV#hello').text(data.hello);
+    });
+  });
+  $('#language_code').keypress(function (key) {
+    if (key.which === 13) {
+      const lang = $('#language_code').val();
+      $.ajax({
+        url: `https://hellosalut.stefanbohacek.dev/?lang=${lang}`,
+        type: 'GET',
+        dataType: 'json'
+      }).done(function (data) {
+        $('DIV#hello').text(data.hello);
+      });
+    }
+  });
+});
